8d48833d32b743858bd38eb30bf9a4acfb747683
Quang Hung Pham <hungpham307@gmail.com>
2022-11-01 11:50:37 +0700

    chore: update config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '480px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1440px'
+      }
+    },
   },
   plugins: [],
 }
